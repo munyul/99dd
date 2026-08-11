8f77ff9b175cb4b0fa55ff4eb33593ada3547cd5
jihun <kigf59@gmail.com>
2026-08-11 18:16:39 +0900

    Fix Vite build config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/99dd/', // 추가
-})
-export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/99dd/', // 추가
 })
